ee1a4832c0048a578bbd3b019511e37ea3724ffe
THEParaence <128623447+THEPearaence@users.noreply.github.com>
2024-03-26 11:34:54 -0300

    Service layer, component registration

---
+++ course/src/main/java/com/examplou/course/services/UserSevice.java
@@ -0,0 +1,26 @@
+package com.examplou.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.examplou.course.entities.User;
+import com.examplou.course.repositories.UserRepository;
+
+@Service
+public class UserSevice {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> fibdAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
